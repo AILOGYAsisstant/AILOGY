9ebd1345c2fb5bf1787e295ca19970730c1b75ff
quangmytam <cutieeyummyy@gmail.com>
2026-03-05 08:46:15 +0700

    fix the content

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,17 @@ const repoName = process.env.GITHUB_REPOSITORY
   ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
   : '/AILOGY/'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: repoName,
+// })
+
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: repoName,
+  server: {
+    allowedHosts: 'all',
+  }
 })
