0e774e6cbeb0fe42e300caa715cfae76655ae722
quangmytam <cutieeyummyy@gmail.com>
2026-03-06 10:23:28 +0700

    Fix GitHub Pages MIME type error with ./ base & .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/AILOGY/',
+  base: './',
   server: {
     allowedHosts: true,
   }
